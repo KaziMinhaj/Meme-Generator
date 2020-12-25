184f4b16baf2cd3a4843e5132684589e6355c122
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2020-12-26 01:01:35 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import Memegenerator from './Components/Memegenerator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Memegenerator/>
     </div>
   );
 }
